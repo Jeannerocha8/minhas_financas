62c7ae853cff9a98321080982d032c8a42c18c8e
Jeanne Rocha <jeannerocha8@gmail.com>
2022-11-28 20:23:14 -0300

    fixed running docker and create url user

---
+++ back_end/app/Http/Controllers/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserPostRequest;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Show the profile for a given user.
+     *
+     * @param  int  $id
+     * @return \Illuminate\View\View
+     */
+    public function show($id)
+    {
+        return view('user.profile', [
+            'user' => User::findOrFail($id)
+        ]);
+    }
+
+    public function store(UserPostRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json($user, 201);
+    }
+}
