a1332065252e8c4737bd353a920db514b3d1d14b
Jeanne Rocha <jeannerocha8@gmail.com>
2022-12-06 21:52:14 -0300

    login-implementation

--- back_end/app/Http/Controllers/UserController.php
+++ back_end/app/Http/Controllers/UserController.php
@@ -4,30 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UserPostRequest;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    /**
-     * Show the profile for a given user.
-     *
-     * @param  int  $id
-     * @return \Illuminate\View\View
-     */
-    public function show($id)
-    {
-        return view('user.profile', [
-            'user' => User::findOrFail($id)
-        ]);
-    }
-
     public function store(UserPostRequest $request)
     {
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password)
-        ]);
+        try {
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
 
-        return response()->json($user, 201);
+            return response()->json($user, 201);
+        } catch (\Exception $error) {
+            return response()->json($error, 400);
+        }
+
     }
 }
--- back_end/app/Http/Requests/UserPostRequest.php
+++ back_end/app/Http/Requests/UserPostRequest.php
@@ -25,10 +25,31 @@ class UserPostRequest extends FormRequest
     {
         return [
             'name' => 'required|min:3|max:255|string',
-            'email' => 'required|email:rfc,dns',
+            'email' => 'required|unique:users|email:rfc,dns',
             'email_verified_at' => 'nullable|strtotime',
-            'passwoard' => 'required|min:6|max:20|',
+            'password' => 'required|min:6|max:20',
             'remember_token' => 'nullable',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => 'O nome é obrogatório',
+            'name.min' => 'Nome deve conter no minímo 3 caracteres',
+            'name.max' => 'O campo Nome ultrapassou a quantidade de caracteres permitida',
+            'name.string' => 'O campo Nome deve conter apenas letras',
+            'email.required' => 'E-mail é obrigatório',
+            'email.unique' => 'E-mail já cadastrado',
+            'email.email' => 'E-mail inválido',
+            'password.required' => 'Senha obrigatória',
+            'password.min' => 'A senha deve conter no mínimo 6 caracteres',
+            'password.max' => 'A senha deve conter no máximo 20 caracteres',
         ];
     }
 }
